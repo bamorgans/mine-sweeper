157c17a047b8edb7af7c70cd162cdfb7fb7a1202
BobM <bamorgans@yahoo.com>
2018-02-04 14:29:27 -0800

    -Cleaned up dashboard -refactored cell component

--- src/constants.js
+++ src/constants.js
@@ -3,16 +3,8 @@
  */
 
 export const ACTION_TYPE = {
-    SET_LEVEL:  'set level',
+    SET_LEVEL:   'set level',
     UPDATE_GAME: 'update game'
-};
-
-export const CELL_TYPE = {
-    MINE:       'mines',
-    FLAG:       'flags',
-    TEXT:       'text',
-    UNKNOWN:    'unknown',
-    OPEN:       'open'
 };
 
 export const GAME_LEVEL = {
@@ -34,8 +26,20 @@ export const ICONS = {
 };
 
 export const LEVEL_CONFIG = {
-    /*beginner:            {rows:  3, cols:  3, mineCount: 1},*/
+    /*beginner:       {rows:  3, cols:  3, mineCount: 1},*/
     beginner:       {rows:  9, cols:  9, mineCount: 10},
     intermediate:   {rows: 16, cols: 16, mineCount: 40},
     expert:         {rows: 24, cols: 24, mineCount: 99}
+};
+
+export const CELL_COLORMAP = {
+    '-1': 'bkgd-brown',
+    '1': 'bkgd-blue',
+    '2': 'bkgd-green',
+    '3': 'bkgd-red',
+    '4': 'bkgd-violet',
+    '5': 'bkgd-magenta',
+    '6': 'bkgd-cyan',
+    '7': 'bkgd-yellow',
+    '8': 'bkgd-black'
 };
